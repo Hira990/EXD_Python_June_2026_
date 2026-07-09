75efb0b5a7e8639ba82ac81003c9754dfa3042af
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-07-09 12:16:48 +0500

    9 july v2

---
+++ 9July2026/leaves.py
@@ -0,0 +1,45 @@
+employees = [
+    {
+        "id": 101,
+        "name": "Ali",
+        "department": "IT",
+        "joining_date": "15/05/2024",
+        "total_leaves": 20,
+        "used_leaves": 5
+    },
+    {
+        "id": 102,
+        "name": "Ahmed",
+        "department": "HR",
+        "joining_date": "10/02/2023",
+        "total_leaves": 18,
+        "used_leaves": 8
+    },
+    {
+        "id": 103,
+        "name": "Sara",
+        "department": "Finance",
+        "joining_date": "01/08/2022",
+        "total_leaves": 25,
+        "used_leaves": 12
+    },
+    {
+        "id": 104,
+        "name": "Ayesha",
+        "department": "Marketing",
+        "joining_date": "20/11/2024",
+        "total_leaves": 15,
+        "used_leaves": 3
+    }
+]
+
+"""
+1. Add Employee
+2. View Employees
+3. Apply Leave
+4. Remaining Leaves
+5. Employees with Lowest Leaves
+6. Search Employee
+7. Upcoming Work Anniversary
+8. Exit
+"""
