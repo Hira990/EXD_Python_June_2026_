bcc6e9674f43a4d66ffa3c645246101b19bee077
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-07-09 12:13:32 +0500

    9 july v1

---
+++ 9July2026/library.py
@@ -0,0 +1,19 @@
+books = [
+    {
+        "id": 1,  # use uuid here
+        "title": "Python Crash Course",
+        "author": "Eric Matthes",
+    },
+    {
+        "id": 2,    # use uuid here
+        "title": "Java Crash Course",
+        "author": "John Smith",
+    },
+]
+
+"""
+1. Add Book
+2. View Books
+3. Search Book
+7. Exit
+"""
