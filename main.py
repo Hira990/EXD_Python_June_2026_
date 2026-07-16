c3cf01da31b29c166dfb8f3e56a3e862a4369d6d
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-07-16 12:18:56 +0500

    16 july v1

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ list, dict
 for loop, while loop
 if else
 datetime module
+regex
+exception handling
+file handling
 
 
 to do:
-regex
+request module
 nested loops
-exception handling
 pip, virtual environment
+decorators
+oop
+"""
+
+"""
+
+python
+5 se 10 or
+database 10 se 12
+fastapi
+django
+llm
+
 """
