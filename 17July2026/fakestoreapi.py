b4da8901634f5bb027c3093f8cfd4dcc71635045
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-07-17 14:52:26 +0500

    17 july v2

--- 17July2026/fakestoreapi.py
+++ 17July2026/fakestoreapi.py
@@ -1,11 +1,37 @@
 """
 https://fakestoreapi.com/docs
 """
+import requests, os
 
-url = "/products"
+url = "https://fakestoreapi.com/products"
+
+response = requests.get(url)
+
+for product in response.json():
+    print(product["id"])
+    print(product['image'])
+
+    byte_response = requests.get(product['image']).content
+
+    if os.path.exists("product_images1"):
+        with open(f'product_images/{product['id']}.jpg', 'wb') as f:
+            f.write(byte_response)
+        print(f"{product['image']} completed...")
 
 
-# Fetch all products
-# Store there images in a separate folder named product_images
-# image ka name, product k name se rkhna ha
 
+"""
+Theory k lie
+
+kahan pe get, post, put, patch, delete, etc use krni hain
+
+
+main status codes pta honey chahie
+200
+201 created
+404 not found
+500 internal server error
+422 unprocessable entity
+400 bad request
+
+"""
