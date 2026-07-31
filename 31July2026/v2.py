b173107cd7f6d8ccc032d40c644ef59e046ba0b7
Chaudhry Iftikhar Hussain <chaudhryiftikharhussain@outlook.com>
2026-07-31 12:47:44 +0500

    ...

--- 31July2026/v2.py
+++ 31July2026/v2.py
@@ -1,3 +1,13 @@
 # Read the file student.xml
 
 # convert it to json and show all the info
+
+student = {
+    "name": "iftikhar",
+    "email": "iftikhar@gmail.com",
+    "add": "lahore"
+}
+print(student.items())
+
+for key, value in student.items():
+    print(f"{key}: {value}")
